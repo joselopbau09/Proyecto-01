ddf0914db9922db44ae3715a30e270636db5f748
Jose Luis Bautista <joseluis27@ciencias.unam.mx>
2022-09-11 16:34:06 -0500

    Pruebas unitarias fianlizadas

---
+++ src/datos/testRequeast.py
@@ -0,0 +1,22 @@
+import unittest
+from getDatos import Request
+
+class TestRequest(unittest.TestCase):
+    
+    def test_getClimaCiudad(self):
+        respuesta = Request()
+        respuesta.coonectarApi('GDL')
+        
+        self.assertIsNotNone(respuesta.getClimaCiudad())
+        self.assertEqual(respuesta.getClimaCiudad()['name'], 'Miguel Hidalgo')
+        self.assertEqual(respuesta.getClimaCiudad()['id'], 3995938)
+        self.assertDictEqual(respuesta.getClimaCiudad()['coord'], {'lon': -103.311, 'lat': 20.5218})
+        
+        respuesta.coonectarApi('MEX')
+        
+        self.assertDictEqual(respuesta.getClimaCiudad()['coord'], {'lon': -99.0721, 'lat': 19.4363})
+        self.assertEqual(respuesta.getClimaCiudad()['name'], 'Pantitlán')
+        self.assertEqual(respuesta.getClimaCiudad()['id'], 3521944)
+
+if __name__ == '__main__':
+    unittest.main()
