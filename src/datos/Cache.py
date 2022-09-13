1b24360977b992a93fe7fd7c5fbfc453ae433baa
Erick Iram García Velasco <erick@sonica.ipv6.infinitum.net.mx>
2022-09-13 12:37:34 -0500

    Correcciones al mostrar datos

--- src/datos/Cache.py
+++ src/datos/Cache.py
@@ -3,23 +3,21 @@ import time
 class Cache:
     
     def agregaDatos(cache, listaDeDatos, identificador):
-        actual = time.time()
+        horaActual = time.time()
         if(cache.get(identificador) == None):
             cache[identificador] = listaDeDatos
-        elif(actual - listaDeDatos[7] > 6000000):
+        elif(horaActual - listaDeDatos[6] > 600000):
             cache[identificador] = listaDeDatos
         elif():
             pass
 
     def muestraDatos(cache, identificador):
-        print("El clima actual es: \n")
         print("El clima es: " + cache[identificador][0])
-        print("Descripción: " + cache[identificador][1])
-        print("La temperatura actual es: " + cache[identificador][2])
-        print("La temperatura mínima es: " + cache[identificador][3])
-        print("La temperatura máxima es: " +  cache[identificador][4])
-        print("La velocidad del viento es: " + cache[identificador][5])
-        print("La nubosidad es del: " + cache[identificador][6])
+        print("La temperatura actual es: " + cache[identificador][1])
+        print("La temperatura mínima es: " + cache[identificador][2])
+        print("La temperatura máxima es: " +  cache[identificador][3])
+        print("La velocidad del viento es: " + cache[identificador][4])
+        print("La nubosidad es del: " + cache[identificador][5])
 
     
 
--- src/datos/getDatos.py
+++ src/datos/getDatos.py
@@ -15,18 +15,18 @@ class Request:
         url = "http://api.openweathermap.org/data/2.5/weather?"
         latitudLongitud = self.ciudades.getCoordenadas(claveCiudad)
         
-        urlCompleta = f'{url}lat={latitudLongitud[0]}&lon={latitudLongitud[1]}&appid={apiKey}'
+        urlCompleta = f'{url}lat={latitudLongitud[0]}&lon={latitudLongitud[1]}&units=metric&lang=sp&appid={apiKey}'
         respuesta = requests.get(urlCompleta)
         self.climaCiudad = json.loads(respuesta.text)
     
     def generaDatos(self):
         datos = []
         detallesClima = self.climaCiudad['weather']
-        datos.append(detallesClima[-1]["main"])
+        #datos.append(detallesClima[-1]["main"])
         datos.append(detallesClima[-1]["description"])
-        datos.append(str(self.climaCiudad["main"]["temp"]) + " °K")
-        datos.append(str(self.climaCiudad["main"]["temp_min"]) + " °K")
-        datos.append(str(self.climaCiudad["main"]["temp_max"]) + " °K")
+        datos.append(str(self.climaCiudad["main"]["temp"]) + " °C")
+        datos.append(str(self.climaCiudad["main"]["temp_min"]) + " °C")
+        datos.append(str(self.climaCiudad["main"]["temp_max"]) + " °C")
         datos.append(str(self.climaCiudad["wind"]["speed"]) + " m/s")
         datos.append(str(self.climaCiudad["clouds"]["all"]) + "%")
         hora = time.time()
@@ -37,9 +37,10 @@ class Request:
 def main():
     a = Request()
     cache = {}
-    a.coonectarApi('MEX')
+    a.coonectarApi('GDL')
     datos = a.generaDatos()
-    Cache.agregaDatos(cache, datos, 'MEX')
-    Cache.muestraDatos(cache, 'MEX')
+    Cache.agregaDatos(cache, datos, 'GDL')
+    Cache.muestraDatos(cache, 'GDL')
+
 main()   
 
