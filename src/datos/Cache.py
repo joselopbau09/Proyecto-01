5fe0bf3152b30814c179ea41c99becda51dde64c
Erick Iram García Velasco <erick@sonica.ipv6.infinitum.net.mx>
2022-09-13 13:04:43 -0500

    Documentación de la clase cache y Request

--- src/datos/Cache.py
+++ src/datos/Cache.py
@@ -1,8 +1,19 @@
 import time
 
 class Cache:
-    
+
+    """Clase para definir el comportamiento del cache
+
+    """
+
     def agregaDatos(cache, listaDeDatos, identificador):
+        """Método para agregar datos al cache, verifica si ya están y su antigüedad.
+
+        Args:
+            cache (dict): diccionario que representa el cache
+            listaDeDatos (list): lista que contiene la información del clima 
+            identificador (str): key para el diccionario
+        """
         horaActual = time.time()
         if(cache.get(identificador) == None):
             cache[identificador] = listaDeDatos
@@ -12,6 +23,13 @@ class Cache:
             pass
 
     def muestraDatos(cache, identificador):
+        """Imprime los datos en la terminal
+        
+        Args:
+            cache (dict): diccionario de donde se extraen los datos
+            identificador (str): key para obtener datos del diccionario
+
+        """
         print("El clima es: " + cache[identificador][0])
         print("La temperatura actual es: " + cache[identificador][1])
         print("La temperatura mínima es: " + cache[identificador][2])
--- src/datos/Request.py
+++ src/datos/Request.py
@@ -4,12 +4,19 @@ import requests, json
 import time
 
 class Request:
+    """Clase para realizar la llamada a la API
+    """
     
     def __init__(self):
         self.ciudades = LecturaCiudades()
         self.climaCiudad = {}
 
     def coonectarApi(self, claveCiudad):
+        """Realiza la llamada a la API y genera el json. Lee un archivo txt con la llave.
+
+        Args:
+            claveCiudad (str): identificador único para obtener las coordenadas de la ciudad
+        """
         archivoKey = open('key.txt')
         apiKey = archivoKey.read()
         url = "http://api.openweathermap.org/data/2.5/weather?"
@@ -20,9 +27,14 @@ class Request:
         self.climaCiudad = json.loads(respuesta.text)
     
     def generaDatos(self):
+        """Guarda los datos del clima en una lista
+
+        Return:
+            list: lista que contiene los datos del clima y la hora en que se realizó la llamada.
+            
+        """
         datos = []
         detallesClima = self.climaCiudad['weather']
-        #datos.append(detallesClima[-1]["main"])
         datos.append(detallesClima[-1]["description"])
         datos.append(str(self.climaCiudad["main"]["temp"]) + " °C")
         datos.append(str(self.climaCiudad["main"]["temp_min"]) + " °C")
